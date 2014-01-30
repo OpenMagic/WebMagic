47929a14be1bf3abc747d113249d40715fb895ff
Tim Murphy <tim@26tp.com>
2014-01-31 10:50:54 +1100

    Cleaned code with Resharper.

--- Projects/WebMagic/WebPages/WebPageHttpHandlerExtensions.cs
+++ Projects/WebMagic/WebPages/WebPageHttpHandlerExtensions.cs
@@ -6,7 +6,8 @@ namespace WebMagic.WebPages
     public static class WebPageHttpHandlerExtensions
     {
         /// <summary>
-        /// Adds a file name extensions to the list of extensions that are processed by the current WebPageHttpHandler instance.
+        ///     Adds a file name extensions to the list of extensions that are processed by the current WebPageHttpHandler
+        ///     instance.
         /// </summary>
         /// <param name="extensions">The extensions to add, without a leading period.</param>
         public static void RegisterExtensions(IEnumerable<string> extensions)
