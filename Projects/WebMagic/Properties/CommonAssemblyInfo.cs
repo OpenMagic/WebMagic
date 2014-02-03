4a6d686dd1df1ed038302da6936cef379334b84d
Tim Murphy <tim@26tp.com>
2014-02-04 08:33:06 +1100

    Changed AssemblyInfo.cs to use CommonAssemblyInfo.cs.

---
+++ Projects/WebMagic/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------------
+// This CommonAssemblyInfo.cs is shared by all projects in this solution. 
+//
+// The master copy is in WebMagic project, all other projects link to this
+// file.
+// -----------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("WebMagic")]
+[assembly: AssemblyCompany("Tim Murphy")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2014")]
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(false)]
+
+// MyGet Build Services stamps the version number.
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
