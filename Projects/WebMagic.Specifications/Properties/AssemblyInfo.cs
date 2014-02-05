2c257a3bba4cde79d9c2a3d33eb3cda9e0eaa63b
Tim Murphy <tim@26tp.com>
2014-02-05 13:13:16 +1100

    Changed Anotar to log minimal messages.

--- Projects/WebMagic.Specifications/Properties/AssemblyInfo.cs
+++ Projects/WebMagic.Specifications/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 ﻿using System.Reflection;
+using Anotar.CommonLogging;
 
 [assembly: AssemblyTitle("WebMagic.Specifications")]
 [assembly: AssemblyDescription("Specifications & tests for WebMagic project.")]
+
+[assembly: LogMinimalMessage]
--- Projects/WebMagic/Properties/AssemblyInfo.cs
+++ Projects/WebMagic/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using Anotar.CommonLogging;
 
 [assembly: AssemblyTitle("WebMagic")]
 [assembly: AssemblyDescription("Collection of web specific extensions and classes by Tim Murphy.")]
+
+[assembly: LogMinimalMessage]
