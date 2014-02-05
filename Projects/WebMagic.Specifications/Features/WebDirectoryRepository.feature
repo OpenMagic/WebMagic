b5d928d56f57e23be92a4c5435e6fe2f1b45a722
Tim Murphy <tim@26tp.com>
2014-02-06 03:44:27 +1100

    Added tests for WebDirectoryRepository.DirectoryExists.

---
+++ Projects/WebMagic.Specifications/Features/WebDirectoryRepository.feature
@@ -0,0 +1,10 @@
+﻿Feature: WebDirectoryRepository
+
+Scenario Outline: DirectoryExists(directory) always returns false
+	Given WebDirectoryRepository(<rootDirectory>)
+	When I call WebDirectoryRepository.DirectoryExists(<directory>)
+	Then <expected> boolean should be returned
+	Examples:
+		| rootDirectory | directory    | expected |
+		| .\            | abc          | false    |
+		| .\            | string.Empty | false    |
---
+++ Projects/WebMagic.Specifications/Steps/WebDirectoryRepositorySteps.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+using WebMagic.Repository;
+using WebMagic.Specifications.Infrastructure;
+
+namespace WebMagic.Specifications.Steps
+{
+    [Binding]
+    public class WebDirectoryRepositorySteps : StepsBase
+    {
+        private bool ActualResult;
+        private WebDirectoryRepository WebDirectoryRepository;
+
+        [Given(@"WebDirectoryRepository\((.*)\)")]
+        public void GivenWebDirectoryRepository(string rootDirectory)
+        {
+            WebDirectoryRepository = new WebDirectoryRepository(rootDirectory);
+        }
+
+        [When(@"I call WebDirectoryRepository\.DirectoryExists\((.*)\)")]
+        public void WhenICallWebDirectoryRepository(string directory)
+        {
+            if (directory == "string.Empty")
+            {
+                directory = string.Empty;
+            }
+
+            ActualResult = WebDirectoryRepository.DirectoryExists(directory);
+        }
+
+        [Then(@"(.*) boolean should be returned")]
+        public void ThenFalseBooleanShouldBeReturned(bool expectedResult)
+        {
+            expectedResult.Should().Be(ActualResult);
+        }
+    }
+}
+
+
